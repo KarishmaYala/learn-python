91f773941149d339cfa9b547508edd7d07fddcca
KarishmaYala <97927035+KarishmaYala@users.noreply.github.com>
2022-01-27 17:03:18 +1100

    Add files via upload
    BODMAS

---
+++ task1.py
@@ -0,0 +1,11 @@
+import sys
+
+#single statement without using any variables
+print('The Output is ', (int(input('Enter first number: ')) + int(input('Enter second number: '))))
+print('The Output is ', (int(input('Enter first number: ')) - int(input('Enter second number: '))))
+print('The Output is ', (int(input('Enter first number: ')) * int(input('Enter second number: '))))
+print('The Output is ', (int(input('Enter first number: ')) / int(input('Enter second number: '))))
+
+# BODMAS
+print('The Output is', (int(input('Enter first number: ')) + int(input('Enter second number: ')) / int(input('Enter third number: '))))
+print('The Output is', (int(input('Enter first number: ')) * int(input('Enter second number: ')) - int(input('Enter third number: '))))
