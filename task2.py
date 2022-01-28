e82582cfcc887c3a9d9a50bddaf1dda678d133e7
KarishmaYala <97927035+KarishmaYala@users.noreply.github.com>
2022-01-28 11:43:31 +1100

    Add files via upload
    Palindrome

---
+++ task2.py
@@ -0,0 +1,16 @@
+
+def is_palindrome(value):
+    #processing special chars and spaces to consider only normal chars for Palindrome.
+    value = ''.join(value.split())
+    value = value.replace('.', '')
+    value = value.lower()
+    return value == value[::-1]
+
+print(is_palindrome(input('Enter the value: ')),
+      is_palindrome(input('Enter the value: ')),
+      is_palindrome(input('Enter the value: ')),
+      is_palindrome(input('Enter the value: ')))
+
+
+
+
